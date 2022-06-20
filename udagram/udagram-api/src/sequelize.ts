eb39c61f67b8692b6d7a3402766eeb9e5af3e329
toseefdc360 <toseef@afnanilab.com>
2022-06-21 00:34:46 +0500

    eb cli orb

--- udagram/udagram-api/src/sequelize.ts
+++ udagram/udagram-api/src/sequelize.ts
@@ -1,12 +1,17 @@
 import { Sequelize } from "sequelize-typescript";
 import { config } from "./config/config";
 
+console.log(config.host)
+console.log(config.database)
+console.log(config.username)
+console.log(config.password)
+
 export const sequelize = new Sequelize({
   username: config.username,
   password: config.password,
   database: config.database,
   host: config.host,
-
+  
   dialect: "postgres",
   storage: ":memory:",
 });
