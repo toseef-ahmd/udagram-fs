6489f23a1e904bbb31fa147819ba296ae604f079
toseefdc360 <toseef@afnanilab.com>
2022-06-21 16:53:54 +0500

    sss

--- udagram/set_env.sh
+++ udagram/set_env.sh
@@ -1,21 +1,21 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=postgres
+export POSTGRES_PASSWORD=postgres
+export POSTGRES_HOST=udagram-db.cchzc2rqkvw8.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=udagram_db
+export AWS_BUCKET=arn:aws:s3:::udagram-tauseef
+export AWS_REGION=us-east-1
+export AWS_PROFILE=default
+export JWT_SECRET=myjwt-secret
+export URL=udagrameb-env.eba-ez8tyq7a.us-east-1.elasticbeanstalk.com
+
 # export POSTGRES_USERNAME=postgres
 # export POSTGRES_PASSWORD=postgres
-# export POSTGRES_HOST=mydbinstance.csxbuclmtj3c.us-east-1.rds.amazonaws.com
-# export POSTGRES_DB=postgres
+# export POSTGRES_HOST=127.0.0.1
+# export POSTGRES_DB=udagram
 # export AWS_BUCKET=arn:aws:s3:::myawsbucket-75139724085
 # export AWS_REGION=us-east-1
 # export AWS_PROFILE=default
 # export JWT_SECRET=mysecretstring
 # export URL=http://localhost:8100
-
-export POSTGRES_USERNAME=postgres
-export POSTGRES_PASSWORD=postgres
-export POSTGRES_HOST=127.0.0.1
-export POSTGRES_DB=udagram
-export AWS_BUCKET=arn:aws:s3:::myawsbucket-75139724085
-export AWS_REGION=us-east-1
-export AWS_PROFILE=default
-export JWT_SECRET=mysecretstring
-export URL=http://localhost:8100
