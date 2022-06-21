beda4d4740bf7030a0e25c18af154a4f481ccf12
toseefdc360 <toseef@afnanilab.com>
2022-06-21 17:08:41 +0500

    sa

--- udagram/udagram-frontend/src/environments/environment.prod.ts
+++ udagram/udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: true,
   appName: "Udagram",
-  apiHost: "http://localhost:8080/api/v0",
+  apiHost: "http://udagram-tauseef.s3-website-us-east-1.amazonaws.com/api/v0",
 };
 
 /*
--- udagram/udagram-frontend/src/environments/environment.ts
+++ udagram/udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://udagram-tauseef.s3-website-us-east-1.amazonaws.com/api/v0'
 };
 
 /*
