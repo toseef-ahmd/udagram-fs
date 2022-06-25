bb378e98cf52f664e14a1430076742cc3a768d88
toseefdc360 <toseef@afnanilab.com>
2022-06-25 20:46:41 +0500

    cors enabled

--- udagram/udagram-api/bin/deploy.sh
+++ udagram/udagram-api/bin/deploy.sh
@@ -1,1 +1,1 @@
-eb setenv POSTGRES_HOST=aa1wz6vry70ecbs.cqazaltaeijp.us-east-1.rds.amazonaws.com POSTGRES_DB=ebdb POSTGRES_USERNAME=postgres POSTGRES_PASSWORD=postgres
+eb setenv POSTGRES_HOST=aa1wz6vry70ecbs.cqazaltaeijp.us-east-1.rds.amazonaws.com POSTGRES_DB=ebdb POSTGRES_USERNAME=postgres POSTGRES_PASSWORD=postgres AWS_BUCKET=myudagram
