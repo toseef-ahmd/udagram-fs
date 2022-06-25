7526e779afd2e8d5f29b26b1d8e3f21d05a66709
toseefdc360 <toseef@afnanilab.com>
2022-06-25 17:52:42 +0500

    new env

--- udagram/set_env.sh
+++ udagram/set_env.sh
@@ -2,13 +2,13 @@
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
 export POSTGRES_USERNAME=postgres
 export POSTGRES_PASSWORD=postgres
-export POSTGRES_HOST=aa7vq37ewpfa0m.cchzc2rqkvw8.us-east-1.rds.amazonaws.com:5432
+export POSTGRES_HOST=aa1wz6vry70ecbs.cqazaltaeijp.us-east-1.rds.amazonaws.com:5432
 export POSTGRES_DB=ebdb
-export AWS_BUCKET=arn:aws:s3:::udagram-tauseef
+export AWS_BUCKET=arn:aws:s3:::myudagram
 export AWS_REGION=us-east-1
-export AWS_PROFILE=default
+export AWS_PROFILE=udagram-group
 export JWT_SECRET=myjwt-secret
-export URL=udagram-env.eba-rphhpzpb.us-east-1.elasticbeanstalk.com
+export URL=Udagramapi-env.eba-gsrhr6nm.us-east-1.elasticbeanstalk.com
 
 # export POSTGRES_USERNAME=postgres
 # export POSTGRES_PASSWORD=postgres
