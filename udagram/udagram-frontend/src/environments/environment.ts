f5139068f71dac9b012268257822c01a6870f376
toseefdc360 <toseef@afnanilab.com>
2022-06-25 22:37:48 +0500

    ss

--- udagram/udagram-frontend/src/environments/environment.ts
+++ udagram/udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://udagram-env.eba-rphhpzpb.us-east-1.elasticbeanstalk.com/api/v0'
+  apiHost: 'http://Udagramapi-env.eba-gsrhr6nm.us-east-1.elasticbeanstalk.com/api/v0'
 };
 
 /*
