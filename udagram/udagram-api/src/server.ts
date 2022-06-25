187c1543743d71c1c45f747bfa5ee248c5a1d12f
toseefdc360 <toseef@afnanilab.com>
2022-06-25 23:06:16 +0500

    kjkj

--- udagram/udagram-api/src/server.ts
+++ udagram/udagram-api/src/server.ts
@@ -44,6 +44,7 @@ import { V0_FEED_MODELS, V0_USER_MODELS } from "./controllers/v0/model.index";
     // Pass to next layer of middleware
     next();
   });
+  
 
   app.use("/api/v0/", IndexRouter);
 
