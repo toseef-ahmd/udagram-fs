3964d1d22a074e637096e5d0ec7cd8db0a60e855
toseefdc360 <toseef@afnanilab.com>
2022-07-01 18:13:23 +0500

    circleci

--- udagram/udagram-api/src/controllers/v0/users/routes/auth.router.ts
+++ udagram/udagram-api/src/controllers/v0/users/routes/auth.router.ts
@@ -108,11 +108,7 @@ router.post('/', async (req: Request, res: Response) => {
 
 
   const jwt = generateJWT(savedUser);
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-  res.header('Access-Control-Allow-Credentials', 'true');
-    
+  
   res.status(201).send({token: jwt, user: savedUser.short()});
 });
 
