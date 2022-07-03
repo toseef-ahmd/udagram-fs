df4bf319fdd38c96941193385710dd30c67455ab
toseefdc360 <toseef@afnanilab.com>
2022-07-03 10:33:46 +0500

    fix

--- udagram/set_env.sh
+++ udagram/set_env.sh
@@ -8,7 +8,7 @@ export AWS_BUCKET=arn:aws:s3:::udagram-tauseef
 export AWS_REGION=us-east-1
 export AWS_PROFILE=default
 export JWT_SECRET=myjwt-secret
-export URL=http://http://udagram-api-dev2222.us-east-1.elasticbeanstalk.com
+export URL=http://udagram-api-dev2222.us-east-1.elasticbeanstalk.com
 
 # export POSTGRES_USERNAME=postgres
 # export POSTGRES_PASSWORD=postgres
